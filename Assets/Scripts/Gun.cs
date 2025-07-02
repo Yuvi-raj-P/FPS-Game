9da6f3607babf2d601d5e3e3704b965f43ed1f0a
Yuvraj P <yuvilds@gmail.com>
2025-07-02 19:02:21 -0400

    New Weapon system and more weapons

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
@@ -8,18 +9,63 @@ public class Gun : MonoBehaviour
 
     public Camera fpsCam;
     public GameObject muzzleFlash;
+    public float muzzleFlashDuration = 0.05f;
+    public float minMuzzleFlashDuration = 0.05f;
+    public float maxMuzzleFlashDuration = 0.15f;
     public GameObject impactEffect;
 
+
+    //Shooting
+    public bool isShooting, readyToShoot;
+    bool allowReset = true;
+    public float shootingDelay = 2f;
+
+    //Burst
+    public int bulletsPerBurst = 3;
+    public int currentBurst;
+
+    public float spreadIntensity;
+
+    public enum ShootingMode
+    {
+        Single,
+        Burst,
+        Auto
+    }
+    public ShootingMode currentShootingMode;
+
+    void Awake()
+    {
+        muzzleFlash.SetActive(false);
+        readyToShoot = true;
+        currentBurst = bulletsPerBurst;
+    }
     void Update()
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        if (currentShootingMode == ShootingMode.Auto)
         {
-            Shoot();
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else if (currentShootingMode == ShootingMode.Single || currentShootingMode == ShootingMode.Burst)
+        {
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+        if (readyToShoot && isShooting)
+        {
+            currentBurst = bulletsPerBurst;
+            ShootGun();
         }
     }
-    void Shoot()
+    IEnumerator Shoot()
     {
-        muzzleFlash.Play();
+
+        readyToShoot = false;
+        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
+
+         muzzleFlash.transform.localRotation = Quaternion.Euler(Random.Range(0f, 360f), -90, 0);
+        muzzleFlash.SetActive(true);
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
@@ -27,5 +73,10 @@ public class Gun : MonoBehaviour
             Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             
         }
+
+        float randomDuration = Random.Range(minMuzzleFlashDuration, maxMuzzleFlashDuration);
+        yield return new WaitForSeconds(randomDuration);
+        muzzleFlash.SetActive(false);
     }
 }
+
